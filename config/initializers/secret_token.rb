d96b54cfcae84b32de946c77c4d55a6c907d7e57
Pablo Fernandez <heelhook@littleq.net>
2013-03-20 23:25:48 -0400

    first commit with rails generated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pitchr::Application.config.secret_key_base = 'd11a3aa4b42044d63b48a6102df19b46486c585e237f37d37e24ec2a5154b7adc9554e6fa3efd5e28e53f771ed5860ba45cd28081e122a69c6f1e767c56778d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pitchr::Application.config.session_store :encrypted_cookie_store, key: '_pitchr_session'
